67cfaa722025b2f9144f13149a3180b842782124
GamerOrSome <82025367+GamerOrSome@users.noreply.github.com>
2025-11-24 13:51:46 +0100

    Changed flash.ld

--- STM_program/Core/Src/main.c
+++ STM_program/Core/Src/main.c
@@ -114,9 +114,9 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
     HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_SET);
-    HAL_Delay(100);
+    HAL_Delay(1000);
     HAL_GPIO_WritePin(LD3_GPIO_Port, LD3_Pin, GPIO_PIN_RESET);
-    HAL_Delay(100);
+    HAL_Delay(1000);
 
     HAL_GPIO_WritePin(LD4_GPIO_Port, LD4_Pin, GPIO_PIN_SET);
     HAL_Delay(100);
@@ -124,9 +124,9 @@ int main(void)
     HAL_Delay(100);
 
     HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_SET);
-    HAL_Delay(100);
+    HAL_Delay(1000);
     HAL_GPIO_WritePin(LD5_GPIO_Port, LD5_Pin, GPIO_PIN_RESET);
-    HAL_Delay(100);
+    HAL_Delay(1000);
 
     HAL_GPIO_WritePin(LD6_GPIO_Port, LD6_Pin, GPIO_PIN_SET);
     HAL_Delay(100);
